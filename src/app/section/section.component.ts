9418194b2ffeebb3a083f1f2b5dc7b344645502c
leledav <leandro.davatz@gmail.com>
2023-12-22 13:31:55 +0100

    fix: video playing

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {SectionInfoDTO} from "../sectionInfoDTO";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-section',
